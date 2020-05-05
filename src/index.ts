0e7100f67ef251625bd9016bedd03562b5ca9166
Mostafa Elebiary <melebiary@umass.edu>
2020-05-05 01:38:58 -0400

    Changed the structure of the files to for easier static serving and deployment

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 
 // Members API Routes
